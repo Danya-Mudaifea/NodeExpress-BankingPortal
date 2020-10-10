a54f3cc33dd6623662337a07ebec83c6ceac9d2d
Danya-Mudaifea <mudifea.danya@gmail.com>
2020-10-10 17:16:45 +0300

    Completed module 4

--- src/app.js
+++ src/app.js
@@ -20,7 +20,25 @@ app.get('/savings', (req, res) => res.render('account', { account: accounts.savi
 app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
 app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
 
+app.get('/transfer', (req, res) =>  res.render('transfer'));
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
+
+app.post('/transfer', (req, res) => {
+  accounts[req.body.from].balance -= req.body.amount;
+  accounts[req.body.to].balance += parseInt(req.body.amount, 10);
+  let accountsJSON = JSON.stringify(accounts, null, 4)
+  fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+  res.render('transfer', {message: 'Transfer Completed'});
+});
+
+app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
+app.post('/payment', (req, res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount);
+    let accountsJSON = JSON.stringify(accounts, null, 4)
+    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+    res.render('payment', {message: 'Payment Successful', account: accounts.credit});
+});
 
 app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
 
